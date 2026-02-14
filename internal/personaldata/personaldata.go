465ad8139d5a8fde6abd9de27a519fb5b8302d89
Ilyasova Aygel <aygilyasova1@gmail.com>
2026-02-15 02:31:19 +0300

    14

--- internal/personaldata/personaldata.go
+++ internal/personaldata/personaldata.go
@@ -8,6 +8,6 @@ type Personal struct {
 	Height float64
 }
 
-func (p *Personal) Print() {
+func (p Personal) Print() {
 	fmt.Printf("Имя: %s\nВес: %.2f кг.\nРост: %.2f м.\n", p.Name, p.Weight, p.Height)
 }
